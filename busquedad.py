6f9fda6e404f2f8e23797f891aae26697becba3c
didier aragon <aragondidier19@gmail.com>
2025-05-24 15:21:34 -0500

    busquedad

--- busquedad.py
+++ busquedad.py
@@ -1,9 +1,9 @@
-# texto = "colombia es un país del extremo "
-# texto.find("colombia")
+texto = "colombia es un país del extremo "
+texto.find("colombia")
 
-# texto = "la practica hace al maestro"
-# print(texto.find("practica"))
-# print(texto.find("maestro"))
+texto = "la practica hace al maestro"
+print(texto.find("practica"))
+print(texto.find("maestro"))
 
 
 #la busqueda sirve para buscar la coordenada de una palabra o numero dentro de un string o variable
