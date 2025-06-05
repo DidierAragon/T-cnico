d65c77d35ab20e7355c3de6723e69cc41fa56bb8
didier aragon <aragondidier19@gmail.com>
2025-06-05 17:19:41 -0500

    taller de condicionales

---
+++ TallerCondicionales.py
@@ -0,0 +1,33 @@
+# print("ejercicio numero 1")
+# numero=int(input("ingresar un numero entero: "))
+# if numero == 0:
+#     print("el numero es cero")
+# elif numero < 0:
+#     print("el numero es negativo")
+# elif numero > 0:
+#     print("el numero es positivo")
+
+# print("ejercicio numero 2")
+# numero_1=int(input("ingresar un numero entero: "))
+# numero_2=int(input("ingresar un numero entero: "))
+# if numero_1 > numero_2:
+#     print(f"el numero {numero_1} es mayor que {numero_2}")
+# elif numero_2 > numero_1:
+#      print(f"el numero {numero_2} es mayor que {numero_1}")
+
+# print("ejercicio numero 3")
+# numero_3=int(input("ingresar un numero entero: "))
+# par=numero_3 % 2
+# if par == 0:
+#     print("el numero es par")
+# else:
+#     print("el numero es impar")
+
+# print("ejercicio numero 4")
+# numero_4=int(input("ingresar un numero entero: "))
+# if numero_4 < 10 and numero_4 < 20:
+#     print(f"el numero {numero_4} no esta entre 10 y 20")
+# else:
+#     print(f"el numero {numero_4}  esta entre 10 y 20")
+
+print("ejercicio numero 5")
