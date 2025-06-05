4fbc517cff46547049da2c2979aae68b2e14c309
didier aragon <aragondidier19@gmail.com>
2025-06-05 15:38:49 -0500

    calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 numero_1=float(input("ingresar primer numero: "))
 numero_2=float(input("ingresar segundo numero: "))
-print("ingresar numero correspondiente a la operacion:")
-print("operaciones:" \
+print(f"ingresar numero correspondiente a la operacion:")
+print(f"operaciones:" \
 "1 Suma" \
 "2 Resta" \
 "3 Multiplicacion" \
@@ -14,4 +14,14 @@ multi=numero_1 * numero_2
 divi=numero_1 // numero_2
 expo=numero_1 ** numero_2 
 if operacion == 1:
-    print("la suma de {numero_1} y {numero_2} es {suma}")
+    print(f"la suma de {numero_1} y {numero_2} es {suma}")
+elif operacion == 2:
+    print(f"la resta de {numero_1} y {numero_2 } es {resta} ")
+elif operacion == 3:
+    print(f"la multiplicacion de {numero_1} y {numero_2} es {multi}")
+elif operacion == 4:
+    print(f"la division de {numero_1} y {numero_2} es {divi}")
+elif operacion == 5:
+    print(f"el numero {numero_1} elevado a {numero_2} es {expo}")
+else:
+    print(f"no ingresaste ningun numero pedido")
