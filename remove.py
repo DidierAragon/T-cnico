883a38473a612bf1149fac9cc1306d868e0cd892
didier aragon <aragondidier19@gmail.com>
2025-06-05 17:27:24 -0500

    remove

--- remove.py
+++ remove.py
@@ -2,8 +2,10 @@
 # liksta.remove("coco")
 # print(liksta)
 #remove sirve para eliminar un dato de la lista
-numeros=[1,2,3,2]
-numeros[2]=4
 
-numeros.remove(4)
-print(numeros)
+
+# numeros=[1,2,3,2]
+# numeros[2]=4
+
+# numeros.remove(4)
+# print(numeros)
