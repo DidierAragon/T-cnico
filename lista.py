f42abc09356dd97e5a7f3e54ec9baac070b6b7d6
didier aragon <aragondidier19@gmail.com>
2025-06-05 17:33:50 -0500

    lista

--- lista.py
+++ lista.py
@@ -1,3 +1,4 @@
-lista=["perro",1,True,"caballo",4.5]
+lista=["perrS",1,True,"caballo",4.5]
+
 print(lista)
 #una lista es una estructura de datos que se utiliza para almacenar multiples elementos en una sola variable (es muy utilizable)
