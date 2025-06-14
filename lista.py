fe7bf02797cc880ee648cff8095f27db7542676b
didier aragon <aragondidier19@gmail.com>
2025-06-14 15:02:53 -0500

    Taller condicionales

--- lista.py
+++ lista.py
@@ -1,4 +1,11 @@
-lista=["perrS",1,True,"caballo",4.5]
+# lista=["perrS",1,True,"caballo",4.5]
 
-print(lista)
-#una lista es una estructura de datos que se utiliza para almacenar multiples elementos en una sola variable (es muy utilizable)
+# print(lista)
+# #una lista es una estructura de datos que se utiliza para almacenar multiples elementos en una sola variable (es muy utilizable)
+
+c = ["amarillo","rosa","rojo"]
+if c[1] == "azul":
+    c.insert(1,"rosa")
+else:
+    print(c)
+print(c)
