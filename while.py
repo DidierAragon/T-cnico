dafd22a38ff8be6c698b9b3421f39a00927dea28
didier aragon <aragondidier19@gmail.com>
2025-06-26 15:13:31 -0500

    while

---
+++ while.py
@@ -0,0 +1,8 @@
+contador = 1
+
+while contador <= 100:
+    print("contador :", contador)
+    contador += 1
+
+
+#es una estructura de control de flujo que se utiliza para repetir un bloque de codigo mientras se cumpla una #condicion
