f282794ab242113f079c5bff10b7e91ceaafd217
didier aragon <aragondidier19@gmail.com>
2025-06-28 17:23:35 -0500

    cajero automatico

--- CajeroAutomatico.py
+++ CajeroAutomatico.py
@@ -1,17 +1,18 @@
 saldo = 0
 print("bienvenido a su cuenta porfaor ingrese el numero da lo que quiere realizar:")
-print("1 ver saldo")
-print("2 depositar saldo")
-print("3 retirar saldo")
-usu = int(input("ingresar numero: "))
+print("1: ver saldo\n2: depositar\n3: retirar\n4: salir")
 
-if usu == 1:
-    print(f"tu saldo actual es de {saldo}")
-elif usu == 2:
-    valor=float(input("ingreasr cantidad que quiere depositar: "))
-    print(f"su saldo queda en {saldo + valor}")
-elif usu == 3:
-    va=float(input("ingrese valor que quiere retirar: "))
-    print(f"su saldo queda en {saldo - va}")
-else:
-    print("error")
+while True:
+    usu = int(input("ingresar numero: "))
+    if usu == 4:
+        print("ha cerrado secion ")
+        break
+    elif usu == 1:
+        print(f"tu saldo actual es de {saldo}")
+    elif usu == 2: 
+        valor=float(input("ingreasr cantidad que quiere depositar: "))
+        print(f"su saldo queda en {saldo + valor}")
+    elif usu == 3: 
+        va=float(input("ingrese valor que quiere retirar: "))
+        print(f"su saldo queda en {saldo - va}")
+    
